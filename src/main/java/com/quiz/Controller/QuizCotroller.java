eb17927a2ef5c715f01966b69843cb4f364e95a0
Avadhuut <avadhutbhosale2001@gmail.com>
2025-06-26 18:28:26 +0530

    Implementing Ratelimiter Annotation using Ressillience4j

--- src/main/java/com/quiz/Controller/QuizCotroller.java
+++ src/main/java/com/quiz/Controller/QuizCotroller.java
@@ -3,6 +3,7 @@ package com.quiz.Controller;
 import com.quiz.model.Quiz;
 import com.quiz.service.QuizService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +30,8 @@ public class QuizCotroller {
     // @CircuitBreaker(name="questionBreaker",fallbackMethod = "questionFallback")
     //Get All Quiz
     @GetMapping()
-    @Retry(name = "quizRetry", fallbackMethod = "questionFallback")
+   // @Retry(name = "quizRetry", fallbackMethod = "questionFallback")
+    @RateLimiter(name="quizRateLimiter",fallbackMethod = "questionFallback")
     public List<Quiz> Get(){
         return quizService.get();
     }
